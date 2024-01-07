cbeb542a9165a45abf3f9ecec94c63acaa3b51aa
Manish Chavan <145777097+manish2120@users.noreply.github.com>
2024-01-07 19:51:04 +0530

    Work In Progress

---
+++ Apple Clone/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  // slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
