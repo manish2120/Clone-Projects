010e6c5ddf1881d656920a996bc8874adf6e3a51
Manish Chavan <145777097+manish2120@users.noreply.github.com>
2024-01-11 10:52:07 +0530

    Add files via upload

---
+++ Myntra Clone/Myntra Basic Functional Clone Page/data.js
@@ -0,0 +1,113 @@
+let items = [
+  {
+  id: '001',
+  image: "images/1.jpg",
+  rating: {
+    stars: 4.5,
+    noOfReviews: 1400
+  },
+  company_name: "Carlton London",
+  item_name: "Rhodium-Plated CZ Floral Studs",
+  current_price: 606,
+  original_price: 1045,
+  discount_percentage: 42
+},
+
+{
+  id: '002',
+  image: "images/2.jpg",
+  rating: {
+    stars: 4.3,
+    noOfReviews: 24
+  },
+  company_name: "CUKOO",
+  item_name: "Women Padded Halter Neck Swimming Dress",
+  current_price: 1507,
+  original_price: 2599,
+  discount_percentage: 42
+},
+
+{
+  id: '003',
+  image: "images/3.jpg",
+  rating: {
+    stars: 4.1,
+    noOfReviews: 249
+  },
+  company_name: "NUEVOSDAMAS",
+  item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
+  current_price: 495,
+  original_price: 1599,
+  discount_percentage: 69
+},
+
+{
+  id: '004',
+  image: "images/4.jpg",
+  rating: {
+    stars: 5.0,
+    noOfReviews: 10
+  },
+  company_name: "Carlton London",
+  item_name: 'Indian Cricket ODI Jersey',
+  current_price: 999,
+  original_price: 999,
+  discount_percentage: 0
+},
+
+{
+  id: '005',
+  image: "images/5.jpg",
+  rating: {
+    stars: 4.5,
+    noOfReviews: 1.4
+  },
+  company_name: "Carlton London",
+  item_name: "Rhodium-Plated CZ Floral Studs",
+  current_price: 606,
+  original_price: 1045,
+  discount_percentage: 42
+},
+
+{
+  id: '006',
+  image: "images/6.jpg",
+  rating: {
+    stars: 0.0,
+    noOfReviews: 0
+  },
+  company_name: "Nike",
+  item_name: 'Men ReactX Running Shoes',
+  current_price: 14995,
+  original_price: 14995,
+  discount_percentage: 0
+},
+
+{
+  id: '007',
+  image: "images/7.jpg",
+  rating: {
+    stars: 4.2,
+    noOfReviews: 388
+  },
+  company_name: "Carlton London",
+  item_name: "Rhodium-Plated CZ Floral Studs",
+  current_price: 639,
+  original_price: 1599,
+  discount_percentage: 60
+},
+
+{
+  id: '008',
+  image: "images/8.jpg",
+  rating: {
+    stars: 4.2,
+    noOfReviews: 5200
+  },
+  company_name: "Nivea",
+  item_name: 'Men Fresh Deodrant 150ml',
+  current_price: 142,
+  original_price: 285,
+  discount_percentage: 50
+}
+]
