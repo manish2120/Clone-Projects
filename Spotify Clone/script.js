09689f17e4b803d971db637861668e26d7b9c3fc
Manish Chavan <manish.chavan113@gmail.com>
2024-01-15 10:56:55 +0530

    updating

---
+++ Spotify Clone/script.js
@@ -0,0 +1,36 @@
+async function getSong() {
+  let songfiles = await fetch("http://127.0.0.1:5500/songs/");
+
+  let response = await songfiles.text();
+  let div = document.createElement('div');
+  div.innerHTML = response;
+  let as = div.getElementsByTagName('a');
+
+  let songs = [];
+
+  for(let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if(element.href.endsWith('.m4a')) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  return songs;
+}
+
+async function main() {
+  let songs = await getSong();
+
+  const songsUL = document.querySelector('.songsList').getElementsByTagName('ul')[0];
+  for (const song of songs) {
+    songsUL.innerHTML = songsUL.innerHTML + `<li>${song.replaceAll('%20', ' ')}</li>`;
+  }
+
+  let audio = new Audio(songs[0]);
+  // audio.play();
+  
+  audio.addEventListener('loadeddata', () => {
+    console.log(audio.duration, audio.currentSrc, audio.currentTime)
+  })
+}
+
+main();
