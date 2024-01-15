726eefa9f62673f01aaacc79e3346a6d7a01a942
Manish Chavan <manish.chavan113@gmail.com>
2024-01-15 12:27:27 +0530

    Seekbar Added

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -2,15 +2,15 @@ async function getSong() {
   let songfiles = await fetch("http://127.0.0.1:5500/songs/");
 
   let response = await songfiles.text();
-  let div = document.createElement('div');
+  let div = document.createElement("div");
   div.innerHTML = response;
-  let as = div.getElementsByTagName('a');
+  let as = div.getElementsByTagName("a");
 
   let songs = [];
 
-  for(let i = 0; i < as.length; i++) {
+  for (let i = 0; i < as.length; i++) {
     const element = as[i];
-    if(element.href.endsWith('.m4a')) {
+    if (element.href.endsWith(".m4a")) {
       songs.push(element.href.split("/songs/")[1]);
     }
   }
@@ -20,17 +20,20 @@ async function getSong() {
 async function main() {
   let songs = await getSong();
 
-  const songsUL = document.querySelector('.songsList').getElementsByTagName('ul')[0];
+  const songsUL = document
+    .querySelector(".songsList")
+    .getElementsByTagName("ul")[0];
   for (const song of songs) {
-    songsUL.innerHTML = songsUL.innerHTML + `<li>${song.replaceAll('%20', ' ')}</li>`;
+    songsUL.innerHTML =
+      songsUL.innerHTML + `<li>${song.replaceAll("%20", " ")}</li>`;
   }
 
   let audio = new Audio(songs[0]);
   audio.play();
-  
-  audio.addEventListener('loadeddata', () => {
-    console.log(audio.duration, audio.currentSrc, audio.currentTime)
-  })
+
+  audio.addEventListener("loadeddata", () => {
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  });
 }
 
 main();
