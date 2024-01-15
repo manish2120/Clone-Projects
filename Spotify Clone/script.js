2adf384ab3b9659a0215ae3d845f0fb66b0bcdfa
Manish Chavan <manish.chavan113@gmail.com>
2024-01-15 17:35:01 +0530

    music added

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -1,7 +1,7 @@
 async function getSong() {
-  let songfiles = await fetch("http://127.0.0.1:5500/songs/");
+  let songFiles = await fetch("http://127.0.0.1:5500/Spotify%20Clone/songs/");
 
-  let response = await songfiles.text();
+  let response = await songFiles.text();
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
@@ -17,6 +17,11 @@ async function getSong() {
   return songs;
 }
 
+function playMusic(track) {
+  let audio = new Audio("/songs/" + track);
+  audio.play();
+}
+
 async function main() {
   let songs = await getSong();
 
@@ -25,15 +30,32 @@ async function main() {
     .getElementsByTagName("ul")[0];
   for (const song of songs) {
     songsUL.innerHTML =
-      songsUL.innerHTML + `<li>${song.replaceAll("%20", " ")}</li>`;
+      songsUL.innerHTML +
+      `<li>
+      <div class="play-song">
+      <i class="fa-solid fa-music"></i>
+      <div class="info">
+      ${song.replaceAll("%20", " ")}
+      </div>
+      <i class="fa-regular fa-circle-play"></i>
+      </div>
+      </li>`;
   }
 
-  let audio = new Audio(songs[0]);
-  audio.play();
+  // let audio = new Audio(songs[0]);
+  // audio.play();
 
-  audio.addEventListener("loadeddata", () => {
-    console.log(audio.duration, audio.currentSrc, audio.currentTime);
-  });
+  // audio.addEventListener("loadeddata", () => {
+  //   console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  // });
 }
+
+Array.from(document.querySelector('.songsList').getElementsByTagName('li')).forEach(e => {
+  e.addEventListener('click', element => {
+    console.log(e.querySelector('.info').firstElementChild.innerHTML);
+  
+    playMusic(e.querySelector('.info').firstElementChild.innerHTML.trim());
+  })
+})
 
 main();
